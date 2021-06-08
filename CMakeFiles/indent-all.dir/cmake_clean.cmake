41aa14dd60ca85a4e6cfe76f8caa761f7765c560
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-06-08 05:55:27 +0000

    clear

--- CMakeFiles/indent-all.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/indent-all"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/indent-all.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/python_bindings.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/python_bindings"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/python_bindings.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
